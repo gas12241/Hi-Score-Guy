34f37d63688bf2b5cfa768b26e225157636db5e1
George <alvarado-salinas.g@northeastern.edu>
2021-03-05 01:10:31 -0800

    I was in a sadboi type of vibe.  Don't know if I feel better after having written all this.  Am tired and will probably sleep soon too.

---
+++ Writing/Dream2.py
@@ -0,0 +1,24 @@
+'''
+
+This is another dream I had, this time with somone I actually know.  It was a friend's little sister.
+
+'''
+
+dream = ("It's been a while since I have thought about this dream and it's"
+        "nothing too special which is what makes is sadder when I think about it."
+        "\nIn my dream I was at an amusement park.  \nI couldn't tell you for sure "
+        "what park it was but part of me thinks it was disneyland(I think someone "
+        "may have been wearing mickey mouse ears or maybe I even saw the mouse "
+        "himself, but something made me think I was in there).\nI was resting"
+        "near a bench when I felt a tap on my shoulder.\nI turned to see her "
+        "standing there looking at me with the goofiest grin and an ice cream "
+        "cone in each hand.\nShe extends the cone my way and I couldn't tell "
+        "you what melted faster, the ice cream in the blistering summer sun,"
+        "or my heart in the moment.")
+
+def main():
+
+    print(dream)
+
+if __name__ == "__main__":
+    main()
---
+++ Writing/dream1.py
@@ -0,0 +1,46 @@
+'''
+
+I had this dream about a girl, she was only in it for 5 seconds tops
+
+'''
+anime_thought = "I just finished watching Hi Score Girl and it struck somethi"
+anime_thought2 = "ng in me.\nI was thinking about why I hadn't made any conne"
+anime_thought3 = "ctions with anyone like Haruo had with Ono.\nI thought it w"
+anime_thought4 = "ould have been cute to go on arcade dates with someone I lo"
+anime_thought5 = "ve.\nIm sad right now listening to living sinlge but I am "
+anime_thought6 = "hopeful one day I will find somone who loves me and pushes"
+anime_thought7 = " me in games."
+
+
+dream = ("I had this one dream a couple nights ago and it stuck with me.\nIn my "
+        "dream I was going to an Ultimate Frisbee game out of the state, or at "
+        "least it felt that way.\nWe left in a charter bus "
+        "and we ended up in a snowy region away from home.  We got to the game "
+        "and I didn't play many points, but I didn't mind too "
+        "much.\nI was happy to just be a part of the team.\nMy dream goes dark "
+        "but somehow I ended up in bed with "
+        "a beautiful woman.\nActually, now that I think about it, it wasn't re"
+        "ally a bed, rather an air mattress, which at the time made sense beca"
+        "use we were at an away game.\nAs we stared at each other, her lips "
+        "failed to contain the smile that had been building within.\nIn her "
+        "eyes I found a calmness and in that moment I knew everything would be ok"
+        "\nFor a second I knew what it felt to be loved")
+        # "be with me, enjoying the moment as much if not more than I was.\n"
+        # " As I stared into her eyes, I wondered, is this what it felt like "
+        # "to be loved?")
+
+
+def main():
+    print(anime_thought + anime_thought2 + anime_thought3 + anime_thought4 + anime_thought5 + anime_thought6 + anime_thought7)
+
+
+    i = 0
+    while i < 3:
+        print(" \n")
+        i += 1
+
+
+    print(dream)
+
+if __name__ == "__main__":
+    main()
